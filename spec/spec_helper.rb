e80bdcd62e04a3b64c1844c9780880bb3db254f7
Hassan Zamani <hsn.zamani@gmail.com>
2014-07-14 12:30:45 +0430

    prepare test db before testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,15 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'rake'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+Dummy::Application.load_tasks
+
+Rake::Task['db:test:prepare'].invoke
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
