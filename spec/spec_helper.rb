8dedb9e7a9e9fd4ea58ab2286a2ff2a7c56063e4
Hassan Zamani <hsn.zamani@gmail.com>
2014-07-14 16:44:23 +0430

    add repo to coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'coveralls'
+Coveralls.wear!
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'rake'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+require 'rake'
 Dummy::Application.load_tasks
-
 Rake::Task['db:test:prepare'].invoke
 
 RSpec.configure do |config|
