3b51a28b668f4404f3d199616a87494b2d38cb74
Hassan Zamani <hsn.zamani@gmail.com>
2014-07-18 16:39:27 +0430

    fix I18n deprecation

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -5,6 +5,8 @@ require 'active_record/railtie'
 Bundler.require
 require 'acts_as_relation'
 
+I18n.enforce_available_locales = false
+
 module Dummy
   class Application < Rails::Application
   end
